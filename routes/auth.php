0233188876a0841eaabd40f8af359be4891691c5
vahag <vahag.kostanyan974@gmail.com>
2024-02-24 22:39:51 +0400

    test

--- routes/auth.php
+++ routes/auth.php
@@ -9,9 +9,9 @@ use App\Http\Controllers\api\Auth\RegisteredUserController;
 use App\Http\Controllers\api\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-// Route::post('/register', [RegisteredUserController::class, 'store'])
-//         ->middleware('guest')
-//         ->name('register');
+Route::post('/register', [RegisteredUserController::class, 'store'])
+        ->middleware('guest')
+        ->name('register');
 
 // Route::post('/login', [AuthenticatedSessionController::class, 'store'])
 //                 ->middleware('guest')
