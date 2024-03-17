b085a95904d5da44621a817f8df86523364c3a57
vahag <vahag.kostanyan974@gmail.com>
2024-03-17 14:59:31 +0400

    add convert route function

--- app/Helpers/PermissionHelper.php
+++ app/Helpers/PermissionHelper.php
@@ -12,6 +12,9 @@ use App\Models\SubPermission;
 
 function hasPermission(int $role_id, string $route): bool
 {
+ 
+    $route = convertRoute($route);
+
     $methods = ['GET' => 'read', 'POST' => 'create', 'PUT' => 'update', 'PATCH' => 'update', 'DELETE' => 'delete'];
 
     $permission_id = Permission::where('page', $route)->where('method', $methods[request()->method()])->first()->id;
@@ -22,10 +25,28 @@ function hasPermission(int $role_id, string $route): bool
         if(!$permission_id) return false;
     }
 
-
     $role_permission = RolePermission::where('role_id', $role_id)->where('permission_id', $permission_id)->first();
     
     if($role_permission) return true;
     
     return false;
+}
+
+function convertRoute(string $route) : string
+{
+    if(request()->method() == 'PUT' || request()->method() == 'PATCH' || request()->method() == 'GET'){
+        $routeArray = explode('/', ltrim($route, '/'));
+        $route = '';
+        foreach($routeArray as $item){
+            if(is_numeric($item)){
+                if(request()->method() == 'GET'){
+                    $route .= '/' . ':id';
+                }
+            }else{
+                $route .= '/' . $item;
+            }
+        }
+    }
+
+    return $route;
 }
