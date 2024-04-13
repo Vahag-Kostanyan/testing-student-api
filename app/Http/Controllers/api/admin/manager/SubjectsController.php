25565f8697e5764121da9ca926503fb5db3dc861
vahag <vahag.kostanyan974@gmail.com>
2024-04-13 22:54:33 +0400

    add subjects controller with acctions and add permission list with getMe acction

---
+++ app/Http/Controllers/api/admin/manager/SubjectsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\api\admin\manager;
+use App\Http\Controllers\core\ApiCrudController;
+use App\Http\Controllers\core\ApiCrudInterface;
+use App\Http\Requests\api\ValidationTrate\admin\manager\SubjectsValidationTrate;
+use App\Models\Subject;
+
+class SubjectsController extends ApiCrudController implements ApiCrudInterface
+{
+    use SubjectsValidationTrate;
+    protected $modelClass = Subject::class;
+    protected $searchFaild = ['id', 'name'];
+}
