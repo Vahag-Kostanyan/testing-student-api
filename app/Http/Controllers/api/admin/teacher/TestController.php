d46ac55c60418c30a652a4ca1dd7c5415d28ef00
HovhannesGhukasyanWeb <hovhannes.ghukasyan@ekeng.am>
2024-05-26 21:43:54 +0400

    Users

--- app/Http/Controllers/api/admin/teacher/TestController.php
+++ app/Http/Controllers/api/admin/teacher/TestController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\api\admin\teacher;
+
 use App\Http\Controllers\core\ApiCrudController;
 use App\Http\Controllers\core\ApiCrudInterface;
 use App\Http\Requests\api\ValidationTrait\admin\teacher\TestValidationTrait;
@@ -11,5 +12,5 @@ class TestController extends ApiCrudController implements ApiCrudInterface
     use TestValidationTrait;
     protected $modelClass = Test::class;
     protected $searchField = ['id', 'name', 'created_at', 'updated_at'];
-    protected $allowedIncludes = ['type', 'user', 'testUsers', 'user.userProfile', 'subject', 'testQuestions', 'testQuestions.question'];
+    protected $allowedIncludes = ['type', 'user', 'testUsers', 'testUsers.user', 'testUsers.user.userProfile', 'user.userProfile', 'subject', 'testQuestions', 'testQuestions.question'];
 }
