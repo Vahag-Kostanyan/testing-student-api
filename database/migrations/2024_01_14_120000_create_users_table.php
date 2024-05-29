ec48bc1b0a41951071f28fc9cd5810b2f4c3d805
vahagn <vahag.kostanyan974@gmail.com>
2024-05-29 14:44:39 +0400

    unset softDelete on users model

--- database/migrations/2024_01_14_120000_create_users_table.php
+++ database/migrations/2024_01_14_120000_create_users_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->foreignId('role_id');
             $table->rememberToken();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
